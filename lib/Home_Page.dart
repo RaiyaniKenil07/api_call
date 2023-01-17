7e29f190d496189c84743efd506cb44ff9db0673
Kenil <kenilraiyani95@gmail.com>
2023-01-17 12:11:37 +0530

    api_calling

---
+++ lib/Home_Page.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'Model/model.dart';
+
+class HttpService {
+  final String postURL = "https://jsonplaceholder.typicode.com/posts";
+
+  Future<List<User>> getPosts() async {
+    Response res = await get(Uri.parse(postURL));
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<User> posts = body
+          .map(
+            (dynamic item) => User.fromJson(item),
+          )
+          .toList();
+      return posts;
+    } else {
+      throw ("Unable to retrieve posts");
+    }
+  }
+}
---
+++ lib/Model/model.dart
@@ -0,0 +1,22 @@
+class User {
+  final int? userId;
+  final int? id;
+  final String? title;
+  final String? body;
+
+  User({
+    this.userId,
+    this.id,
+    this.title,
+    this.body,
+  });
+
+  User.fromJson(Map<String, dynamic> json)
+      : userId = json['userId'] as int?,
+        id = json['id'] as int?,
+        title = json['title'] as String?,
+        body = json['body'] as String?;
+
+  Map<String, dynamic> toJson() =>
+      {'userId': userId, 'id': id, 'title': title, 'body': body};
+}
